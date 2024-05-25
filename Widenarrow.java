cc92e2cafd602a32a616afefc9a3ad49521a4d81
soumyaathanimath <168898037+soumyaathanimath@users.noreply.github.com>
2024-05-25 12:29:56 +0530

    wide

---
+++ Widenarrow.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package widenarrow;
+
+/**
+ *
+ * @author dell1
+ */
+public class Widenarrow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        package widenarrowdemo;
+
+public class WideNarrow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b=10;
+		short s=10;
+		int i=10;
+		long l=10;
+		float f=10;
+		double d=10;
+		char c=10;
+		boolean bl=true;
+
+	}
+
+}
